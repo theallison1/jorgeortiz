bf2f0dfb53edd6435a9b4dbdef45211ef4d1a606
nicolas espinosa <nico.2011.pr@gmail.com>
2026-03-19 08:34:52 -0300

    cambios

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const unidades = [
+    {
+      id: 1,
+      marca: "TOYOTA",
+      modelo: "Hilux 2.8 SRX 4x4",
+      anio: 2023,
+      precio: "u$s 44.500",
+      img: "https://images.unsplash.com/photo-1583121274602-3e2820c69888?auto=format&fit=crop&q=80&w=800", // Imagen de ejemplo
+      categoria: "Camionetas",
+      destacado: true
+    },
+    {
+      id: 2,
+      marca: "HONDA",
+      modelo: "CB300F Twister",
+      anio: 2024,
+      precio: "$ 6.200.000",
+      img: "https://images.unsplash.com/photo-1558981403-c5f91cbba527?auto=format&fit=crop&q=80&w=800",
+      categoria: "Motos",
+      destacado: false
+    },
+    {
+      id: 3,
+      marca: "VOLKSWAGEN",
+      modelo: "Amarok V6 Extreme",
+      anio: 2022,
+      precio: "u$s 39.000",
+      img: "https://images.unsplash.com/photo-1606577924006-27d39b132ee6?auto=format&fit=crop&q=80&w=800",
+      categoria: "Camionetas",
+      destacado: true
+    }
+  ];
